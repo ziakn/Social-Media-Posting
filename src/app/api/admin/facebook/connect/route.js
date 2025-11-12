cc44e5215bb184c6cea86d90102fd9cc53cc452e
xiakn <z.muhammad@daralsharq.net>
2025-11-12 15:19:54 +0300

    fire base permissions

--- src/app/api/admin/facebook/connect/route.js
+++ src/app/api/admin/facebook/connect/route.js
@@ -4,7 +4,26 @@ export async function GET() {
   const redirect_uri = process.env.FB_REDIRECT_URI;
   const app_id = process.env.FB_APP_ID;
 
-  const authUrl = `https://www.facebook.com/v24.0/dialog/oauth?client_id=${app_id}&redirect_uri=${encodeURIComponent(redirect_uri)}&scope=email,public_profile,instagram_basic,pages_show_list`;
+  const scopes = [
+    "email",
+    "public_profile",
+    "instagram_basic",
+    "pages_show_list",
+    "pages_manage_posts",
+    "pages_read_engagement",
+    "pages_manage_engagement",
+    "pages_manage_metadata",
+    "pages_read_user_content",
+    "pages_manage_ads",
+    "ads_management",
+    "business_management",
+    "instagram_manage_comments",
+    "instagram_manage_insights",
+  ];
+
+  const authUrl = `https://www.facebook.com/v24.0/dialog/oauth?client_id=${app_id}&redirect_uri=${encodeURIComponent(
+    redirect_uri
+  )}&scope=${scopes.join(",")}`;
 
   return NextResponse.redirect(authUrl);
 }
