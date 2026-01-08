28476c2a46658b72e58d8eda436b626091b77c28
xiakn <z.muhammad@daralsharq.net>
2026-01-08 14:29:01 +0300

    blue sky module

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+      },
+      {
+        protocol: 'https',
+        hostname: 'bsky.social',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.bsky.app',
+      }
+    ],
+  },
   serverActions: {
     bodySizeLimit: '1000mb', // or 20mb if you want
   },
